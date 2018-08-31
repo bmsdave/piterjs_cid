7f137350a66e4e5e2a292a022938a03bafef7d53
Vadim Gorbachev (bmsdave) <bmsdave@gmail.com>
2018-08-31 11:45:08 +0300

    configure gemini

---
+++ .gemini.js
@@ -0,0 +1,12 @@
+module.exports = {
+    rootUrl: 'https://bmsdave.github.io/piterjs_cid/',
+    gridUrl: 'https://bmsdave.github.io/piterjs_cid/',
+
+    browsers: {
+        chrome: {
+            desiredCapabilities: {
+                browserName: 'chrome'
+            }
+        }
+    }
+};
